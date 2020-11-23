e901746f87fdf4951838f347a5f6923286b24cfa
Shanil Lobanwala <slobanwala@ad.thinkmcs.com>
2020-11-23 06:08:43 -0600

    Finished listing routes

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import datetime as dt
 
 # Routes:
 # / - Home page, List all routes that are available.
-# /api/v1.0/precipitation - dictionary wit date(key), prcp(value), and returns JSON of dict
+# /api/v1.0/precipitation - dictionary with date(key), prcp(value), and returns JSON of dict
 # /api/v1.0/stations - JSON list of stations
 # /api/v1.0/tobs - JSON list of temperature observations for prev. year of most active station.
 # /api/v1.0/<start> - JSON list of min temp, avg temp and max temp for given start date
@@ -33,6 +33,25 @@ station = database.classes.station
 session_link = Session(engine)
 
 app = Flask(__name__)
+
+# Routes:
+@app.route("/")
+def main():
+    """List all routes that are available."""
+    return (
+        f"Hawaii Temperature Analysis by Shanil Lobanwala<br/>"
+        f"Available Routes:<br/><br/>"
+        f"dictionary with date(key), prcp(value), and returns JSON of dict:<br/>"
+        f"/api/v1.0/precipitation<br/><br/>"
+        f"JSON list of stations:<br/>"
+        f"/api/v1.0/stations<br/><br/>"
+        f"JSON list of temperature observations for prev. year of most active station:<br/>"
+        f"/api/v1.0/tobs<br/><br/>"
+        f"JSON list of min temp, avg temp and max temp for given start date:<br/>"
+        f"/api/v1.0/<start><br/><br/>"
+        f"JSON list of min temp, avg temp and max temp for given start date or end date:<br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
 
 if __name__ == "__main__":
     app.run(debug = True)
